e1bc8b84ef20f0adbd31dae00e1c50674bbd85e8
Prince0302 <115176604+Prince0302@users.noreply.github.com>
2025-08-05 15:48:22 +0530

    Update Mini-Max Sum.cpp

--- week1/Mini-Max Sum/Mini-Max Sum.cpp
+++ week1/Mini-Max Sum/Mini-Max Sum.cpp
@@ -1,1 +1,14 @@
+void miniMaxSum(vector<int> arr) {
+    long total = 0;
+    int minVal = arr[0];
+    int maxVal = arr[0];
+
+    for (int num : arr) {
+        total += num;
+        if (num < minVal) minVal = num;
+        if (num > maxVal) maxVal = num;
+    }
+
+    cout << total - maxVal << " " << total - minVal << endl;
+}
 
